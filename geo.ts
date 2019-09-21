9497fc6c790a6cbc69ca4e7d9b921c088ae746f6
Nick Drane <nicholasdrane@gmail.com>
2019-09-20 20:26:11 -0600

    add district search functionality

---
+++ geo.ts
@@ -0,0 +1,45 @@
+import * as shapefile from "shapefile";
+import { Feature, Geometry, GeoJsonProperties } from "geojson";
+import * as path from "path";
+import * as turf from "@turf/turf";
+
+type Coordinates = [number, number];
+
+export class DistrictLookup {
+  districts?: Array<Feature<Geometry, GeoJsonProperties>>;
+
+  async loadDistrictData() {
+    const dataDir = path.join("__dirname", "..", "data");
+    try {
+      const featureCollection = await shapefile.read(
+        path.join(dataDir, "arcgisDistricts", "tl_2018_us_cd116.shp"),
+        path.join(dataDir, "arcgisDistricts", "tl_2018_us_cd116.dbf")
+      );
+      this.districts = featureCollection.features;
+    } catch (err) {
+      console.error("failed to load district data");
+      process.exit(1);
+    }
+  }
+
+  findDistrict(coords: Coordinates) {
+    if (!this.districts) {
+      throw new Error(
+        "Must call loadDistrictData before searching the district data set"
+      );
+    }
+    const point = turf.point(coords);
+    for (const district of this.districts) {
+      // There should be any features of other types, but this typeguard satisfies the compiler
+      if (district.geometry.type === "Polygon") {
+        const polygon = turf.polygon(
+          district.geometry.coordinates,
+          district.properties
+        );
+        if (turf.booleanPointInPolygon(point, polygon)) {
+          console.log(polygon.properties);
+        }
+      }
+    }
+  }
+}
