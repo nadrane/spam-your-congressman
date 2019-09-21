75504d7136eaa5c46bf14bdf2b12ad3004271b4b
Nick Drane <nicholasdrane@gmail.com>
2019-09-20 20:56:52 -0600

    script produces json line file of congressmen

--- scripts/scrapeCongressmen.sh
+++ scripts/scrapeCongressmen.sh
@@ -2,4 +2,6 @@
 
 cd data
 touch congressmen.json
-curl "https://api.propublica.org/congress/v1/116/house/members.json" -H "X-API-Key: $PROPUBLICA_API_TOKEN" > congressmen.json
+curl "https://api.propublica.org/congress/v1/116/house/members.json" -H "X-API-Key: $PROPUBLICA_API_TOKEN" \
+  | jq '.results[0] | .members | .[]' \
+  > congressmen.json
