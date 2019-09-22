070c30d23f1fce36c2cdd7ecf7f55de44024701a
Nick Drane <nicholasdrane@gmail.com>
2019-09-22 17:09:06 -0600

    add standard bunyan serializers so errors serialize correctly

--- src/logging.ts
+++ src/logging.ts
@@ -3,5 +3,6 @@ import * as env from "./env";
 
 export default bunyan.createLogger({
   name: "spam-your-congressman",
-  level: env.LOG_LEVEL
+  level: env.LOG_LEVEL,
+  serializers: bunyan.stdSerializers
 });
