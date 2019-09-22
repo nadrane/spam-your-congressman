fe12c673bd631c05a901967a5eb28e75a87fb7b7
Nick Drane <nicholasdrane@gmail.com>
2019-09-22 16:09:41 -0600

    use redis_url env var instead of rediscloud_url

--- src/env.ts
+++ src/env.ts
@@ -6,7 +6,7 @@ export const PORT = process.env.PORT || "1337";
 export const TWILIO_MAKE_CALL_URL =
   process.env.TWILIO_MAKE_CALL_URL || `http:/${os.hostname()}:${PORT}/makeCall`;
 export const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
-export const REDIS_URL = process.env.REDISCLOUD_URL || "redis://localhost:6379";
+export const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 export const ALWAYS_CALL_ME =
   Boolean(process.env.ALWAYS_CALL_ME) === false ? false : true;
 
